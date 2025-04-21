7c47d1926704d9756399d820ab2aad80826d864f
cuson311 <cuson108@gmail.com>
2025-04-21 16:51:45 +0700

    FE: debug when deploy

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,8 @@ import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
+  console.log("API endpoint", process.env.NEXT_PUBLIC_API_ENDPOINT);
+
   return (
     <div className="flex min-h-screen flex-col">
       {/* Hero Section */}
